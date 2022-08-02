695069983918624e63598b902c01c7d400710ae0
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-08-02 13:30:55 -0300

    update: retrieve data from db

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/links", { useNewUrlParser: true });
 
 const linkSchema = new mongoose.Schema({
-  title: {type:String, required:true},
+  title: { type: String, required: true },
   description: String,
-  url: {type:String, required:true},
-  click: {type:Number, default: 0},
+  url: { type: String, required: true },
+  click: { type: Number, default: 0 },
 });
 
 const Link = mongoose.model("Link", linkSchema); // it represents the collection (starts with capital letter)
@@ -20,6 +20,23 @@ let link = new Link({
   url: "https://github.com/guifaxina",
 });
 
+let db = mongoose.connection;
+db.on("err", () => {
+  console.log("Something wrong happened");
+});
+db.once("open", () => {
+  console.log("Banco carregado");
+  app.get("/:title", async (req, res) => {
+    let title = req.params.title;
+    
+    try {
+      let doc = await Link.findOne({ title });
+      res.redirect(doc.url);
+    } catch (err) {
+      res.send(err);
+    }
+  });
+});
 
 link
   .save() // method that saves in the DB.
